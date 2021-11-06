87c497a03d9b12028449cd2782cc2b783d890cd4
Kusarigama <eclip_bl@tutanota.com>
2021-11-06 22:26:44 +0700

    vol 1-4

---
+++ meadowlark.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const handlebars = require('express-handlebars').create({defaultLayout: 'main'});
+const fortune = require('./lib/fortune');
+
+app.engine('handlebars', handlebars.engine);
+app.set('view engine', 'handlebars');
+app.set('port', process.env.PORT || 3000);
+
+app.use(express.static(__dirname + '/public'));
+app.use(function (req, res, next) {
+  res.locals.showTests = app.get('env') !== 'production' && req.query.test === '1';
+  next();
+});
+
+
+app.get('/', function(req, res) {
+  res.render('home');
+});
+
+app.get('/about', function(req, res) {
+  res.render('about', {fortune: fortune.getFortunes});
+});
+
+// page 404
+app.use(function(req, res) {
+  res.status(404);
+  res.render('404');
+});
+
+app.use(function(err, req, res, next) {
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
+});
+
+app.listen(app.get('port'), function() {
+  console.log('Express start on localhost:' + app.get('port'));
+});
